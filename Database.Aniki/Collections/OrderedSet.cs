8a379beeac51d2c2e997c231b52d5eb2ceaee614
Ben Wang <wyqmlm66@gmail.com>
2022-10-12 16:51:26 -0700

    Initial commit

---
+++ Database.Aniki/Collections/OrderedSet.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Database.Aniki.Collections
+{
+    public class OrderedSet<T> : ICollection<T>, IEnumerable<T>, IEnumerable
+    {
+        public OrderedSet() : this(EqualityComparer<T>.Default)
+        {
+        }
+        public OrderedSet(IEqualityComparer<T> comparer)
+        {
+            this.m_Dictionary = new Dictionary<T, LinkedListNode<T>>(comparer);
+            this.m_LinkedList = new LinkedList<T>();
+        }
+        public OrderedSet(T[] items) : this(EqualityComparer<T>.Default)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                this.Add(items[i]);
+            }
+        }
+        public OrderedSet(IEnumerable<T> items) : this(EqualityComparer<T>.Default)
+        {
+            foreach (T item in items)
+            {
+                this.Add(item);
+            }
+        }
+        public OrderedSet(T[] items, IEqualityComparer<T> comparer)
+        {
+            this.m_Dictionary = new Dictionary<T, LinkedListNode<T>>(comparer);
+            this.m_LinkedList = new LinkedList<T>();
+            for (int i = 0; i < items.Length; i++)
+            {
+                this.Add(items[i]);
+            }
+        }
+        public OrderedSet(IEnumerable<T> items, IEqualityComparer<T> comparer)
+        {
+            this.m_Dictionary = new Dictionary<T, LinkedListNode<T>>(comparer);
+            this.m_LinkedList = new LinkedList<T>();
+            foreach (T item in items)
+            {
+                this.Add(item);
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return this.m_Dictionary.Count;
+            }
+        }
+        public virtual bool IsReadOnly
+        {
+            get
+            {
+                return this.m_Dictionary.IsReadOnly;
+            }
+        }
+        void ICollection<T>.Add(T item)
+        {
+            this.Add(item);
+        }
+
+        public int AddRange(IEnumerable<T> items)
+        {
+            int num = 0;
+            foreach (T item in items)
+            {
+                bool flag = this.Add(item);
+                if (flag)
+                {
+                    num++;
+                }
+            }
+            return num;
+        }
+        public void Clear()
+        {
+            this.m_LinkedList.Clear();
+            this.m_Dictionary.Clear();
+        }
+        public bool Remove(T item)
+        {
+            LinkedListNode<T> node;
+            bool flag = this.m_Dictionary.TryGetValue(item, out node);
+            bool flag2 = !flag;
+            bool result;
+            if (flag2)
+            {
+                result = false;
+            }
+            else
+            {
+                this.m_Dictionary.Remove(item);
+                this.m_LinkedList.Remove(node);
+                result = true;
+            }
+            return result;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this.m_LinkedList.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+        public bool Contains(T item)
+        {
+            return this.m_Dictionary.ContainsKey(item);
+        }
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            this.m_LinkedList.CopyTo(array, arrayIndex);
+        }
+        public bool Add(T item)
+        {
+            bool flag = this.m_Dictionary.ContainsKey(item);
+            bool result;
+            if (flag)
+            {
+                result = false;
+            }
+            else
+            {
+                LinkedListNode<T> value = this.m_LinkedList.AddLast(item);
+                this.m_Dictionary.Add(item, value);
+                result = true;
+            }
+            return result;
+        }
+
+        private readonly IDictionary<T, LinkedListNode<T>> m_Dictionary;
+        private readonly LinkedList<T> m_LinkedList;
+    }
+}
---
+++ Database.Aniki/Infrastructure/IDbContextOptions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+
+namespace Database.Aniki.Infrastructure
+{
+    /// <summary>
+    ///     <para>
+    ///         The options to be used by a <see cref="DbContext" />. You normally override
+    ///         <see cref="DbContext.OnConfiguring(DbContextOptionsBuilder)" /> or use a <see cref="DbContextOptionsBuilder" />
+    ///         to create instances of classes that implement this interface, they are not designed to be directly created
+    ///         in your application code.
+    ///     </para>
+    ///     <para>
+    ///         The service lifetime is <see cref="ServiceLifetime.Scoped" />. This means that each
+    ///         <see cref="DbContext" /> instance will use its own instance of this service.
+    ///         The implementation may depend on other services registered with any lifetime.
+    ///         The implementation does not need to be thread-safe.
+    ///     </para>
+    /// </summary>
+    /// <remarks>
+    ///     See <see href="https://aka.ms/efcore-docs-providers">Implementation of database providers and extensions</see>
+    ///     for more information.
+    /// </remarks>
+    public interface IDbContextOptions
+    {
+        /// <summary>
+        ///     Gets the extensions that store the configured options.
+        /// </summary>
+        IEnumerable<IDbContextOptionsExtension> Extensions { get; }
+
+        /// <summary>
+        ///     Gets the extension of the specified type. Returns null if no extension of the specified type is configured.
+        /// </summary>
+        /// <typeparam name="TExtension">The type of the extension to get.</typeparam>
+        /// <returns>The extension, or <see langword="null" /> if none was found.</returns>
+        TExtension? FindExtension<TExtension>()
+            where TExtension : class, IDbContextOptionsExtension;
+    }
+}
